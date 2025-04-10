ba8820c50fbd5ddf21eec6ff14686ea88c70fcbe
acabrera04 <acabrera04164@gmail.com>
2025-04-09 22:32:49 -0400

    Fixed stack issue with func returns

--- main.ml
+++ main.ml
@@ -848,7 +848,7 @@ let main() = (
     | TAC_Assign_FunctionCall(var, mname, Some(args_vars)) ->
       fprintf fout "\t## Dynamic/static dispatch x86 goes here\n";
     | TAC_Assign_Self_FunctionCall(var, mname, cname, Some(args_vars)) ->
-      if !funRetFlag <> "" then (stackOffset := !stackOffset + 8; funRetFlag := "";);
+      if !funRetFlag <> "" && not(List.mem (TAC_Variable(!funRetFlag)) args_vars) then (stackOffset := !stackOffset + 8; funRetFlag := "";);
       fprintf fout "\n\t## %s(...)\n" mname;
       fprintf fout "\tpushq %%r12\n";
       fprintf fout "\tpushq %%rbp\n";
